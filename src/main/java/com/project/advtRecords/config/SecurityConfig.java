58bf4cdfbe68d47b74e25ff493497e6b48dfe88a
adityab9990 <adityabhalerao656@gmail.com>
2026-06-24 16:30:40 +0530

    Modify CORS settings for Vercel deployment
    Updated CORS configuration to allow requests from the Vercel URL and added PATCH method support.

--- src/main/java/com/project/advtRecords/config/SecurityConfig.java
+++ src/main/java/com/project/advtRecords/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.project.advtRecords.config;
 
+
 import java.util.Arrays;
 
 import org.springframework.context.annotation.Bean;
@@ -10,6 +11,7 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
 
 @Configuration
 @EnableWebSecurity
@@ -29,14 +31,16 @@ public class SecurityConfig {
 	    return http.build();
 	}
 
-	@Bean
+	  @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
-        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        
+        // Exactly your Vercel URL
+        configuration.setAllowedOrigins(Arrays.asList("https://advt-frontend.vercel.app"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setAllowCredentials(true);
-        
+
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
         return source;
